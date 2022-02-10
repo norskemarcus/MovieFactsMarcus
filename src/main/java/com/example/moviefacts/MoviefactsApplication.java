a34d78e93aae3ab15038762f9810c3a34d72ed20
jarltuxen <jarl.tuxen@gmail.com>
2022-02-10 10:39:02 +0100

    Initial commit

---
+++ src/main/java/com/example/moviefacts/MoviefactsApplication.java
@@ -0,0 +1,13 @@
+package com.example.moviefacts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviefactsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoviefactsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/moviefacts/MoviefactsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moviefacts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviefactsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
